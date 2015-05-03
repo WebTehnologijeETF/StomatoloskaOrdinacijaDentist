5322bd30baca0ee9f23aa14731bcdc8085e6b1f8
Saudin Ademovic <saudin.12@hotmail.com>
2015-05-03 13:26:27 +0200

    Meni izmjene
    Izmijenjen meni, stil i vecina stranica zbog deformacije u IE

--- Projekat/meni.js
+++ Projekat/meni.js
@@ -1,10 +1,28 @@
 
 function prikaziMeni(){
-    document.getElementById("padajuciMeni").style.display="block";
+    document.getElementById("dropmenu1").style.display="block";
 
 }
 function sakrijMeni(){
-    document.getElementById("padajuciMeni").style.display="none";
+    document.getElementById("dropmenu1").style.display="none";
 }
+
+
+function prikazi2Meni(){
+    document.getElementById("dropmenu2").style.display="block";
+
+}
+function sakrij2Meni(){
+    document.getElementById("dropmenu2").style.display="none";
+}
+function prikazi3Meni(){
+    document.getElementById("dropmenu3").style.display="block";
+
+}
+function sakrij3Meni(){
+    document.getElementById("dropmenu3").style.display="none";
+}
+
+
 
 
